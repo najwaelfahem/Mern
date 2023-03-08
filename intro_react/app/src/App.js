626674d6f1547f976df2383405b4772083b3ad18
elfahemnajwa <elfahem13najwa@gmail.com>
2023-03-08 22:37:15 +0100

    first react with jsx

---
+++ intro_react/app/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <h4>Things I need to do:</h4>
+      <ul>
+        <ol>* Learn react</ol>
+        <ol>* Climb Mt.Everest</ol>
+        <ol>* Run a marathon</ol>
+        <ol>* Feed the dogs</ol>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
