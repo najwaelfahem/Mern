11b025dbc752b072860ccd115947da8dd7aa968b
elfahemnajwa <elfahem13najwa@gmail.com>
2023-03-09 09:49:37 +0100

    corrige

--- Component/pro_it_up/src/components/PersonCard.jsx
+++ Component/pro_it_up/src/components/PersonCard.jsx
@@ -9,16 +9,16 @@ class PersonCard extends Component {
     }
     
   render() {
-     const changeAge = ()=>{
-      
-      this.setState({age:this.props.age + 1});
+    const changeAge = ()=>{
+      console.log("click change", this.state.age);
+      this.setState({age:this.state.age + 1});
   }
     return (
       <div className="App">
           <h1>{this.props.lastName}, {this.props.firstName}</h1>
           <p>Age: {this.state.age}</p>
           <p>Hair Color: {this.props.hairColor}</p>
-          <button onClick={this.changeAge}>Happy Birthday{this.props.firstName} {this.props.lastName}</button>
+          <button onClick={()=>changeAge()}>Happy Birthday{this.props.firstName} {this.props.lastName}</button>
       </div>
   
     )
