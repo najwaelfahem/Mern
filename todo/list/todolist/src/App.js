a10e134df1984b4c3d5157f1f92fa6ef4886f3ae
elfahemnajwa <elfahem13najwa@gmail.com>
2023-03-13 15:32:29 +0100

    fetch

---
+++ todo/list/todolist/src/App.js
@@ -0,0 +1,14 @@
+import data from './Data/todo';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/list/todolist/src/Data/todo.js
@@ -0,0 +1,31 @@
+const data=[
+    {
+    todo:"Learn html",
+    id:1,
+    status:false
+    },
+    {
+    todo:"Learn css",
+    id:2,
+    status:false
+    },
+    {
+    todo:"Learn react",
+    id:3,
+    status:false
+    },
+    {todo:"Learn Vue",
+    id:4,
+    status:false
+    },
+    {todo:"Learn Angular",
+    id:5,
+    status:false
+    },
+    {todo:"Learn Node",
+    id:6,
+    status:false
+    }
+    ]
+    export default data;
+    
